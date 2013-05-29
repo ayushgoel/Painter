866f8c9f7d1ed99ea1608854118f94aa20fce584
Ayush Goel <ayush.g@directi.com>
2013-05-29 13:04:28 +0530

    Fix crash on single tap
    Draw a squiggle only if it has more than 0 points.
    
    Remove extraneous whitespaces and fix indentation.

--- Painter/MainView.m
+++ Painter/MainView.m
@@ -49,14 +49,13 @@
 - (void)drawRect:(CGRect)rect {
   // Drawing code.
   CGContextRef context = UIGraphicsGetCurrentContext();
-  
+
   for (Squiggle *squiggle in self.finishedSquiggles)
     [self drawSquiggle:squiggle inContext:context];
 
-  for (NSString *key in self.squiggles) {
-    Squiggle *squiggle = [self.squiggles valueForKey:key];
-    [self drawSquiggle:squiggle inContext:context];
-  }
+  [self.squiggles enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+    [self drawSquiggle:obj inContext:context];
+  }];
 }
 
 
@@ -66,32 +65,39 @@
   UIColor *squiggleColor = squiggle.strokeColor;
   CGColorRef	colorRef = [squiggleColor CGColor];	// get the CGColor
   CGContextSetStrokeColorWithColor(context, colorRef);
-  
+
   // set the line width to the squiggle's line width
   CGContextSetLineWidth(context, squiggle.lineWidth);
-  
-  NSMutableArray *points = [ squiggle points];	// get points from squiggle
-  
-  // retrieve the NSValue object and store the value in firstPoint
-  CGPoint firstPoint;	// declare a CGPoint
-  [[points objectAtIndex:0] getValue:&firstPoint];
-  
-  // move to the point
-  CGContextMoveToPoint(context, firstPoint.x, firstPoint.y);
-  
-  // draw a line from each point to the next in order
-  for (int i=1; i < [points count]; i++) {
-    NSValue *value = [points objectAtIndex:i];
-    CGPoint point;	// declare a new point
-    [value getValue:&point];	// store the value in point
-    
-    // draw a line to the new point
-    CGContextAddLineToPoint(context, point.x, point.y);
-    
-  } // end for
-  
+
+  NSMutableArray *points = [squiggle points];	// get points from squiggle
+
+  // draw squiggle only if it has more than one point.
+  // Solves crashes.
+  if ([points count] != 0) {
+    // retrieve the NSValue object and store the value in firstPoint
+    CGPoint firstPoint;	// declare a CGPoint
+    [[points objectAtIndex:0] getValue:&firstPoint];
+
+    if ([points count] == 1) {
+      CGPoint dummyPoint = CGPointMake(firstPoint.x + self.lineWidth,
+                                       firstPoint.y + self.lineWidth);
+      NSValue *value = [NSValue valueWithBytes:&dummyPoint objCType:@encode(CGPoint)];
+      [points addObject:value];
+    }
+
+    // move to the point
+    CGContextMoveToPoint(context, firstPoint.x, firstPoint.y);
+
+    // draw a line from each point to the next in order
+    for (NSValue *value in points) {
+      CGPoint point;	// declare a new point
+      [value getValue:&point];	// store the value in point
+
+      // draw a line to the new point
+      CGContextAddLineToPoint(context, point.x, point.y);
+    }
+  }
   CGContextStrokePath(context);
-  UIGraphicsPushContext(context);
 }
 
 
@@ -104,6 +110,7 @@
     NSString *key = [NSString stringWithFormat:@"%@", touchValue];
     // retrieve the squiggle for this touch using the key
     Squiggle *squiggle = [self.squiggles valueForKey:key];
+
     // remove the squiggle from the dictionary and place it in an array
     // of finished squiggles [finishedSquiggles addObject:squiggle];
     // add to finishedSquiggles [squiggles removeObjectForKey:key];
@@ -122,7 +129,7 @@
 }
 
 // determines if this view can become the first responder
-- (BOOL)canBecomeFirstResponder { 
+- (BOOL)canBecomeFirstResponder {
   return YES;
 }
 
@@ -132,38 +139,37 @@
   if (event.subtype == UIEventSubtypeMotionShake){
     // create an alert prompting the user about clearing the painting
     NSString *message = @"Are you sure you want to clear the painting?";
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Clear painting"
-                                                    message:message
-                                                   delegate:self
-                                          cancelButtonTitle:@"Cancel"
-                                          otherButtonTitles:@"Clear", nil];
+    UIAlertView *alert = [[[UIAlertView alloc] initWithTitle:@"Clear painting"
+                                                     message:message
+                                                    delegate:self
+                                           cancelButtonTitle:@"Cancel"
+                                           otherButtonTitles:@"Clear", nil] autorelease];
     [alert show];
-    [alert release];
   }// end if
   [super motionEnded:motion withEvent:event];
 } // end method
 
 // called whenever the user places a finger on the screen
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-  NSArray *array = [ touches allObjects];	// get all the new touches
-  
+  NSArray *array = [touches allObjects];	// get all the new touches
+
   // loop through each new touch
   for (UITouch *touch in array ) {
     // create and configure a new squiggle
     Squiggle *squiggle = [[Squiggle alloc] init];
     [squiggle setStrokeColor:self.color];	// set squiggle's stroke color
     [squiggle setLineWidth:self.lineWidth];	// set squiggle's line width
-    
+
     // add the location of the first touch to the squiggle
-    //[squiggle addPoint:[touch locationInView:self]];
-    
+    [squiggle addPoint:[touch locationInView:self]];
+
     // the key for each touch is the value of the pointer
     NSValue *touchValue = [NSValue valueWithPointer:touch];
     NSString *key = [NSString stringWithFormat:@"%@", touchValue];
-    
+
     // add the new touch to the dictionary under a unique key
     [self.squiggles setValue:squiggle forKey:key];
-    [squiggle release];	// we are done with squiggle so release it	 
+    [squiggle release];	// we are done with squiggle so release it
   }	// end for
 }
 
@@ -171,27 +177,27 @@
 // called whenever the user drags a finger on the screen
 - (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
   NSArray *array = [touches allObjects];	// get all the moved touches
-  
+
   // loop through all the touches
   for (UITouch *touch in array) {
     // get the unique key for this touch
     NSValue * touchValue = [NSValue valueWithPointer:touch];
-    
+
     // fetch the squiggle this touch should be added to using the key
     Squiggle *squiggle = [self.squiggles valueForKey:[NSString stringWithFormat:@"%@", touchValue]];
-    
+
     // get the current and previous touch locations
     CGPoint current = [touch locationInView:self];
     CGPoint previous = [touch previousLocationInView:self];
     [squiggle addPoint:current];	// add the new point to the squiggle
-    
+
     // screen needs to be redrawn
     CGPoint lower, higher;
     lower.x = (previous.x > current.x ? current.x : previous.x);
     lower.y	= (previous.y > current.y ? current.y: previous.y);
     higher.x = (previous.x < current.x ? current.x : previous.x);
     higher.y = (previous.y < current.y ? current.y: previous.y);
-    
+
     // redraw the screen in the required region
     [self setNeedsDisplayInRect:CGRectMake(lower.x - self.lineWidth,
                                            lower.y - self.lineWidth, higher.x - lower.x + self.lineWidth * 2,
